39f0a728cc3c62218d8aca6a863e7765948562ec
arcmee <kihyun3297@gmail.com>
2025-03-27 18:39:14 +0900

    홈 레이아웃

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -6,30 +6,90 @@ class HomePage extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(
           centerTitle: true,
-          title: Text("data"),
+          title: Text("기차예매"),
           backgroundColor: Colors.blueAccent,
         ),
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.amber,
-          ),
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                child: Text('text'),
-              ),
-              Spacer(),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("button")
-              ),
-              SizedBox(height: 50,)
-            ],
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                selectArea(),
+                SizedBox(height: 20,),
+                seatSelectButton(),
+                SizedBox(height: 50,)
+              ],
+            ),
           ),
         ),
+      ),
+    );
+  }
+
+  SizedBox seatSelectButton() {
+    return SizedBox(
+      width: double.infinity,
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: Colors.purple,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          ),
+        ),
+        onPressed: () {},
+        child: Text("좌석 선택",
+        style: TextStyle(
+          color: Colors.white,
+        ),),
+      ),
+    );
+  }
+
+  Container selectArea() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      height: 200,
+      width: double.infinity,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+        selectStation('출발역', '선택'),
+        VerticalDivider(
+          thickness: 3,
+          indent: 70,
+          endIndent: 70,
+        ),
+        selectStation('도착역', '선택'),
+      ],),
+    );
+  }
+
+  Expanded selectStation(String title, String selected) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("$title",
+          style: TextStyle(
+            fontSize: 16,
+          ),),
+          Text("$selected",
+          style: TextStyle(
+            fontSize: 40,
+          ),),
+        ],
       ),
     );
   }
